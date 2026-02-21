56ac990879ae20b6258b326fa82602972e11fdfc
Chris Tessum with Claude <contact@earthscilab.com>
2026-02-21 01:54:50 +0000

    Revert "Fix test to match renamed MW_air -> MW_Air constant"
    This reverts commit 17c8cbac2738a56267adb7d83ef304464dd86195.

--- test/elemental_carbon_test.jl
+++ test/elemental_carbon_test.jl
@@ -24,7 +24,7 @@
 
     obs = string(observed(sys))
     @test occursin(
-        "ElementalCarbon₊NEI2016MonthlyEmis_PEC(t) ~ (MW_Air*nmolpermol*NEI2016MonthlyEmis₊PEC(t)) / MW_C",
+        "ElementalCarbon₊NEI2016MonthlyEmis_PEC(t) ~ (MW_air*nmolpermol*NEI2016MonthlyEmis₊PEC(t)) / MW_C",
         obs
     )
     @test occursin("ElementalCarbon₊T(t) ~ GEOSFP₊I3₊T(t)", obs)
